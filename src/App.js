d0c8fa9ee78fc12076c7d726d418add5540a695c
Justin Bass <justinbass6036@gmail.com>
2023-05-22 23:44:58 -0400

    commit message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import Person from './container/Person';
 
 const App = () => {
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/people')
+    .then((r) => r.json())
+    .then((data) => setPeople(data))
+  }, [])
+
   return (
-  <h2>Test</h2>
+    <Person />
   );
 }
 
