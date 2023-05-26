1f06f725594b2d6b5447de591cd3def688c7334a
Justin Bass <justinbass6036@gmail.com>
2023-05-26 03:23:15 -0400

    commit message

--- src/components/AddErrandForm.js
+++ src/components/AddErrandForm.js
@@ -4,18 +4,29 @@ import { useParams } from "react-router-dom";
 const AddErrandForm = ({currentPerson, addErrand}) => {
 
 
-    const [errand, setErrand] = useState({
-        errands: ""
-    })
+    const [errandData, setErrandData] = useState({
+        errand: "",
+        date: "",
+        time: "",
+        location: "",
+        address: "",
+        commute:""
+})
 
     const handleErrandChange = (e) => {
-        setErrand({[e.target.errands]: e.target.value})
+        const name = e.target.name;
+        const value = e.target.value
+
+        setErrandData({
+            ...errandData,
+            [name]: value,
+        })
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const newErrand = {
-            ...errand,
+            ...errandData,
             person_id: currentPerson.id
         }
         addErrand(newErrand)
@@ -26,10 +37,55 @@ const AddErrandForm = ({currentPerson, addErrand}) => {
         <form onSubmit={handleSubmit}>
             <input
             type="text"
+            name="errand"
             placeholder="Errand..."
-            value={errand.errands}
             onChange={handleErrandChange}
+            value={errandData.errand}
             />
+
+            <input
+            type="text"
+            name="date"
+            placeholder="Date..."
+            onChange={handleErrandChange}
+            value={errandData.date}
+            />
+
+            <input
+            type="text"
+            name="time"
+            placeholder="Time..."
+            onChange={handleErrandChange}
+            value={errandData.time}
+            />
+
+            <input
+            type="text"
+            name="location"
+            placeholder="Location..."
+            onChange={handleErrandChange}
+            value={errandData.location}
+            />
+
+            <input
+            type="text"
+            name="address"
+            placeholder="Address..."
+            onChange={handleErrandChange}
+            value={errandData.address}
+            />
+
+            <input
+            type="text"
+            name="commute"
+            placeholder="Commute..."
+            onChange={handleErrandChange}
+            value={errandData.commute}
+            />
+
+
+
+
 
             <input type="submit"/>
 
