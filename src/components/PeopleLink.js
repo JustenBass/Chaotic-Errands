f7bc5c88ff776ff2baafefc22545d7543a12dc30
Justin Bass <justinbass6036@gmail.com>
2023-05-31 18:30:08 -0400

    commit message

--- src/components/PeopleLink.js
+++ src/components/PeopleLink.js
@@ -5,7 +5,9 @@ const PeopleLink = ({people}) => {
     return(
         <div className="link-div">
             <Link className="link" to={`/people/${people.id}`}>
+                <div className="name-link">
                 <h2>{people.name}</h2>
+                </div>
             </Link>
         </div>
     )
